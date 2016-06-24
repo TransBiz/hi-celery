112a7b7e66d97a928fa7b381ee796d5e917b1fb2
c3h3 <c3h3.tw@gmail.com>
2016-06-24 14:33:19 +0800

    add backend

--- tasks.py
+++ tasks.py
@@ -2,8 +2,9 @@ import os
 from celery import Celery
 
 BROKER_URI = os.environ.get("BROKER_URI", "mongodb://localhost:9000/celeryDemo")
+BACKEND_URI = os.environ.get("BACKEND_URI", "mongodb://localhost:9000/celeryDemoBackend")
 
-app = Celery('tasks', broker=BROKER_URI)
+app = Celery('tasks', broker=BROKER_URI, backend=BACKEND_URI)
 
 @app.task
 def add(x, y):
