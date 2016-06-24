c9b0a0a098c81876562f2b7ef54f2c20c35d3aed
c3h3 <c3h3.tw@gmail.com>
2016-06-24 14:19:31 +0800

    extract BROKER_URI

--- tasks.py
+++ tasks.py
@@ -1,6 +1,9 @@
+import os
 from celery import Celery
 
-app = Celery('tasks', broker='mongodb://localhost:9001/celeryDemo')
+BROKER_URI = os.environ.get("BROKER_URI", "mongodb://localhost:9000/celeryDemo")
+
+app = Celery('tasks', broker=BROKER_URI)
 
 @app.task
 def add(x, y):
